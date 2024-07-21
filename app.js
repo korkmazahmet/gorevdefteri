cdc1620a117ba1c5772913ec0304e6a1de9a3f2a
Ahmet Korkmaz <ahmet_korkmaz95@hotmail.com>
2024-07-21 11:26:45 +0300

    Görev defteri görevleri kaydededer

---
+++ app.js
@@ -0,0 +1,44 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+const listler = document.querySelectorAll("#listt li");
+
+
+
+function addTask() {
+    if(inputBox.value === ''){
+       alert("Bir şeyler yazmalısın")
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.className = "spann";
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+   saveData();
+
+    
+}
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+       e.target.classList.toggle("checked");
+       saveData();
+    }else if(e.target.tagName ===" SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+});
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
